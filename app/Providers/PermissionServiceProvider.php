8796fb0a234e51c09a044d6a8f4b8658ed116238
andrew <andrtv0@gmail.com>
2025-04-01 16:12:33 +0400

    Реализация логики ролей и прав пользователей, создание blade-директив для проверки роли, создание middleware для проверки ролей/прав, создание миграций таблиц ролей и прав а также 3х сводных таблиц, добавление сидеров

---
+++ app/Providers/PermissionServiceProvider.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Providers;
+
+use App\Models\Permission;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\ServiceProvider;
+
+class PermissionServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot()
+    {
+        //Пример вернёт true для текущего пользователя, если ему дано право управлять пользователями
+        //Gate::allows('manage-users');
+        try {
+            Permission::get()->map(function ($permission) {
+                Gate::define($permission->slug, function ($user) use ($permission) {
+                    return $user->hasPermissionTo($permission);
+                });
+            });
+        } catch (\Exception $e) {
+            report($e);
+            return false;
+        }
+    }
+}
