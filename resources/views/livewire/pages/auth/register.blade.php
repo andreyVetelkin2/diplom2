378a6284f21e79d75f598a7f6237bc0dc2c6b9ea
andrew <andrtv0@gmail.com>
2025-04-22 13:55:55 +0400

    Верстка входа и регистрации

--- resources/views/livewire/pages/auth/register.blade.php
+++ resources/views/livewire/pages/auth/register.blade.php
@@ -38,52 +38,107 @@ new #[Layout('layouts.guest')] class extends Component
 }; ?>
 
 <div>
-    <form wire:submit="register">
-        <!-- Name -->
-        <div>
-            <x-input-label for="name" :value="__('Name')" />
-            <x-text-input wire:model="name" id="name" class="block mt-1 w-full" type="text" name="name" required autofocus autocomplete="name" />
-            <x-input-error :messages="$errors->get('name')" class="mt-2" />
+{{--    <form wire:submit="register">--}}
+{{--        <!-- Name -->--}}
+{{--        <div>--}}
+{{--            <x-input-label for="name" :value="__('Name')" />--}}
+{{--            <x-text-input wire:model="name" id="name" class="block mt-1 w-full" type="text" name="name" required autofocus autocomplete="name" />--}}
+{{--            <x-input-error :messages="$errors->get('name')" class="mt-2" />--}}
+{{--        </div>--}}
+
+{{--        <!-- Email Address -->--}}
+{{--        <div class="mt-4">--}}
+{{--            <x-input-label for="email" :value="__('Email')" />--}}
+{{--            <x-text-input wire:model="email" id="email" class="block mt-1 w-full" type="email" name="email" required autocomplete="username" />--}}
+{{--            <x-input-error :messages="$errors->get('email')" class="mt-2" />--}}
+{{--        </div>--}}
+
+{{--        <!-- Password -->--}}
+{{--        <div class="mt-4">--}}
+{{--            <x-input-label for="password" :value="__('Password')" />--}}
+
+{{--            <x-text-input wire:model="password" id="password" class="block mt-1 w-full"--}}
+{{--                            type="password"--}}
+{{--                            name="password"--}}
+{{--                            required autocomplete="new-password" />--}}
+
+{{--            <x-input-error :messages="$errors->get('password')" class="mt-2" />--}}
+{{--        </div>--}}
+
+{{--        <!-- Confirm Password -->--}}
+{{--        <div class="mt-4">--}}
+{{--            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />--}}
+
+{{--            <x-text-input wire:model="password_confirmation" id="password_confirmation" class="block mt-1 w-full"--}}
+{{--                            type="password"--}}
+{{--                            name="password_confirmation" required autocomplete="new-password" />--}}
+
+{{--            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />--}}
+{{--        </div>--}}
+
+{{--        <div class="flex items-center justify-end mt-4">--}}
+{{--            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}" wire:navigate>--}}
+{{--                {{ __('Already registered?') }}--}}
+{{--            </a>--}}
+
+{{--            <x-primary-button class="ms-4">--}}
+{{--                {{ __('Register') }}--}}
+{{--            </x-primary-button>--}}
+{{--        </div>--}}
+{{--    </form>--}}
+
+
+
+    <div class="register-box">
+        <div class="register-logo">
+            Регистрация
         </div>
+        <!-- /.register-logo -->
+        <div class="card">
+            <div class="card-body register-card-body">
+                <p class="register-box-msg">Пожалуйста зарегистрируйтесь</p>
+                <form wire:submit="register" >
+                    <div class="input-group mb-3">
+                        <input wire:model="name" type="text" class="form-control" placeholder="Имя">
+                        <div class="input-group-text"><span class="bi bi-person"></span></div>
+                        <x-input-error :messages="$errors->get('name')" class="mt-2" />
+                    </div>
+                    <div class="input-group mb-3">
+                        <input  wire:model="email" type="email" class="form-control" placeholder="Email">
+                        <div class="input-group-text"><span class="bi bi-envelope"></span></div>
+                        <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                    </div>
+                    <div class="input-group mb-3">
+                        <input wire:model="password" type="password" class="form-control" placeholder="Пароль">
+                        <div class="input-group-text"><span class="bi bi-lock-fill"></span></div>
+                        <x-input-error :messages="$errors->get('password')" class="mt-2" />
+                    </div>
+                    <div class="input-group mb-3">
+                        <input  wire:model="password_confirmation" type="password" class="form-control" placeholder="Подтверждение пароля">
+                        <div class="input-group-text"><span class="bi bi-lock-fill"></span></div>
+                        <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+                    </div>
+                    <!--begin::Row-->
+                    <div class="row">
+                        <!-- /.col -->
+                        <div class="col-4">
+                            <div class="d-grid gap-2">
+                                <button type="submit" class="btn btn-primary">Зарегистрироваться</button>
+                            </div>
+                        </div>
+                        <!-- /.col -->
+                    </div>
+                    <!--end::Row-->
+                </form>
 
-        <!-- Email Address -->
-        <div class="mt-4">
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input wire:model="email" id="email" class="block mt-1 w-full" type="email" name="email" required autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                <!-- /.social-auth-links -->
+                <p class="mb-0">
+                    <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}" wire:navigate>
+                        {{ __('Уже зарегистрированы?') }}
+                    </a>
+                </p>
+            </div>
+            <!-- /.register-card-body -->
         </div>
-
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input wire:model="password" id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-
-            <x-text-input wire:model="password_confirmation" id="password_confirmation" class="block mt-1 w-full"
-                            type="password"
-                            name="password_confirmation" required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}" wire:navigate>
-                {{ __('Already registered?') }}
-            </a>
-
-            <x-primary-button class="ms-4">
-                {{ __('Register') }}
-            </x-primary-button>
-        </div>
-    </form>
+    </div>
 </div>
