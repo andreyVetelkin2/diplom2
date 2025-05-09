619037d619d717b00d19854464bf8c71872c7f13
karl163532456 <118519770+karl163532456@users.noreply.github.com>
2025-05-09 19:00:01 +0400

    Vlad final(Xaxa kogo ja naebivay )
    Исправил ошибки, теперь пашет

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Author;
+use App\Models\User;
 use GuzzleHttp\Client;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Pagination\LengthAwarePaginator;
